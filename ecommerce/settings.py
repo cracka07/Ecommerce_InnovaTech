3a308614ae737b96dd1534369d2cabbc396d5db8
mariano <mariano_fferrer@hotmail.com>
2024-02-01 01:46:09 -0300

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+      'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
@@ -131,6 +132,9 @@ STATIC_ROOT=BASE_DIR/'static'
 STATICFILES_DIRS = [
     'ecommerce/static'
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=BASE_DIR/"media"
