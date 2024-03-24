5ec1f075b852d6d4918a5d2f6783fdc945fc3d5a
mariano <mariano_fferrer@hotmail.com>
2024-03-24 17:28:39 -0300

    saved files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -143,7 +143,8 @@ STATIC_ROOT=BASE_DIR/'static'
 # STATICFILES_DIRS = [
 #     'ecommerce/static'
 # ]
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
