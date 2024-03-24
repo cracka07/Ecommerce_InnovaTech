f06144ff229bf11605ecdef12358ece5051230be
mariano <mariano_fferrer@hotmail.com>
2024-03-24 17:38:42 -0300

    saved files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-# build_files.sh
+set -o errexit
+
 pip install -r requirements.txt
 
-# make migrations
-python manage.py migrate 
-python manage.py collectstatic
+python manage.py collectstatic --no-input
+python manage.py migrate
